f15ddeb4ca9ae1bc33b58c95de1a5de2e0defca5
americast <sayan.sinha@iitkgp.ac.in>
2019-12-16 09:07:28 +0530

    Step 2

--- DRL/custom_gym/envs/custom_env_dir/custom_env.py
+++ DRL/custom_gym/envs/custom_env_dir/custom_env.py
@@ -70,93 +70,123 @@ class CustomEnv(gym.Env):
 
     def step(self, action):
 
-        position = self.state[0]
-        velocity = self.state[1]
-        force = min(max(action[0], -1.0), 1.0)
+        # position = self.state[0]
+        # velocity = self.state[1]
+        # force = min(max(action[0], -1.0), 1.0)
 
-        velocity += force*self.power -0.0025 * math.cos(3*position)
-        if (velocity > self.max_speed): velocity = self.max_speed
-        if (velocity < -self.max_speed): velocity = -self.max_speed
-        position += velocity
-        if (position > self.max_position): position = self.max_position
-        if (position < self.min_position): position = self.min_position
-        if (position==self.min_position and velocity<0): velocity = 0
+        # velocity += force*self.power -0.0025 * math.cos(3*position)
+        # if (velocity > self.max_speed): velocity = self.max_speed
+        # if (velocity < -self.max_speed): velocity = -self.max_speed
+        # position += velocity
+        # if (position > self.max_position): position = self.max_position
+        # if (position < self.min_position): position = self.min_position
+        # if (position==self.min_position and velocity<0): velocity = 0
 
-        done = bool(position >= self.goal_position and velocity >= self.goal_velocity)
+        # done = bool(position >= self.goal_position and velocity >= self.goal_velocity)
 
-        reward = 0
-        if done:
-            reward = 100.0
-        reward-= math.pow(action[0],2)*0.1
+        # reward = 0
+        # if done:
+        #     reward = 100.0
+        # reward-= math.pow(action[0],2)*0.1
 
-        self.state = np.array([position, velocity])
-        return self.state, reward, done, {}
+        # self.state = np.array([position, velocity])
+
+        self.state[3] += action[1][0]
+        self.state[4] += action[1][1]
+
+        self.state[0] += action[0]
+        e_b = self.state[0]
+        e_net = self.state[1]
+        v_T = self.state[2]
+        w_e = self.state[3]
+        w_c = self.state[4]
+        t = self.state[5]
+
+        r_b = 0
+        u = self.sigma * self.v
+        if e_b < self.gamma * self.E2B:
+            r_b = -(e_b * u + self.eps * self.E2B)
+        else:
+            r_b = -(e_b * u + (1 - self.eps) * self.E2B)
+
+        r_net = 0
+        if w_c > 1:
+            r_net = -(e_net * u + self.k2up * (w_c - 1))
+        elif w_c < 0:
+            r_net = -(e_net * u + self.k2low * (0 - w_c))
+        else:
+            r_net = -(e_net * u)
+
+        reward = r_net + r_b
+
+        return self.state, reward, False, {}
 
     def reset(self):
+        # self.state = np.array([self.np_random.uniform(low=-0.6, high=-0.4), 0])
         self.state = np.array([self.np_random.uniform(low=-0.6, high=-0.4), 0])
         return np.array(self.state)
 
 #    def get_state(self):
 #        return self.state
 
-    def _height(self, xs):
-        return np.sin(3 * xs)*.45+.55
+    # def _height(self, xs):
+    #     return np.sin(3 * xs)*.45+.55
 
-    def render(self, mode='human'):
-        screen_width = 600
-        screen_height = 400
+    # def render(self, mode='human'):
+    #     screen_width = 600
+    #     screen_height = 400
 
-        world_width = self.max_position - self.min_position
-        scale = screen_width/world_width
-        carwidth=40
-        carheight=20
+    #     world_width = self.max_position - self.min_position
+    #     scale = screen_width/world_width
+    #     carwidth=40
+    #     carheight=20
 
 
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(screen_width, screen_height)
-            xs = np.linspace(self.min_position, self.max_position, 100)
-            ys = self._height(xs)
-            xys = list(zip((xs-self.min_position)*scale, ys*scale))
+    #     if self.viewer is None:
+    #         from gym.envs.classic_control import rendering
+    #         self.viewer = rendering.Viewer(screen_width, screen_height)
+    #         xs = np.linspace(self.min_position, self.max_position, 100)
+    #         ys = self._height(xs)
+    #         xys = list(zip((xs-self.min_position)*scale, ys*scale))
 
-            self.track = rendering.make_polyline(xys)
-            self.track.set_linewidth(4)
-            self.viewer.add_geom(self.track)
+    #         self.track = rendering.make_polyline(xys)
+    #         self.track.set_linewidth(4)
+    #         self.viewer.add_geom(self.track)
 
-            clearance = 10
+    #         clearance = 10
 
-            l,r,t,b = -carwidth/2, carwidth/2, carheight, 0
-            car = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            car.add_attr(rendering.Transform(translation=(0, clearance)))
-            self.cartrans = rendering.Transform()
-            car.add_attr(self.cartrans)
-            self.viewer.add_geom(car)
-            frontwheel = rendering.make_circle(carheight/2.5)
-            frontwheel.set_color(.5, .5, .5)
-            frontwheel.add_attr(rendering.Transform(translation=(carwidth/4,clearance)))
-            frontwheel.add_attr(self.cartrans)
-            self.viewer.add_geom(frontwheel)
-            backwheel = rendering.make_circle(carheight/2.5)
-            backwheel.add_attr(rendering.Transform(translation=(-carwidth/4,clearance)))
-            backwheel.add_attr(self.cartrans)
-            backwheel.set_color(.5, .5, .5)
-            self.viewer.add_geom(backwheel)
-            flagx = (self.goal_position-self.min_position)*scale
-            flagy1 = self._height(self.goal_position)*scale
-            flagy2 = flagy1 + 50
-            flagpole = rendering.Line((flagx, flagy1), (flagx, flagy2))
-            self.viewer.add_geom(flagpole)
-            flag = rendering.FilledPolygon([(flagx, flagy2), (flagx, flagy2-10), (flagx+25, flagy2-5)])
-            flag.set_color(.8,.8,0)
-            self.viewer.add_geom(flag)
+    #         l,r,t,b = -carwidth/2, carwidth/2, carheight, 0
+    #         car = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+    #         car.add_attr(rendering.Transform(translation=(0, clearance)))
+    #         self.cartrans = rendering.Transform()
+    #         car.add_attr(self.cartrans)
+    #         self.viewer.add_geom(car)
+    #         frontwheel = rendering.make_circle(carheight/2.5)
+    #         frontwheel.set_color(.5, .5, .5)
+    #         frontwheel.add_attr(rendering.Transform(translation=(carwidth/4,clearance)))
+    #         frontwheel.add_attr(self.cartrans)
+    #         self.viewer.add_geom(frontwheel)
+    #         backwheel = rendering.make_circle(carheight/2.5)
+    #         backwheel.add_attr(rendering.Transform(translation=(-carwidth/4,clearance)))
+    #         backwheel.add_attr(self.cartrans)
+    #         backwheel.set_color(.5, .5, .5)
+    #         self.viewer.add_geom(backwheel)
+    #         flagx = (self.goal_position-self.min_position)*scale
+    #         flagy1 = self._height(self.goal_position)*scale
+    #         flagy2 = flagy1 + 50
+    #         flagpole = rendering.Line((flagx, flagy1), (flagx, flagy2))
+    #         self.viewer.add_geom(flagpole)
+    #         flag = rendering.FilledPolygon([(flagx, flagy2), (flagx, flagy2-10), (flagx+25, flagy2-5)])
+    #         flag.set_color(.8,.8,0)
+    #         self.viewer.add_geom(flag)
 
-        pos = self.state[0]
-        self.cartrans.set_translation((pos-self.min_position)*scale, self._height(pos)*scale)
-        self.cartrans.set_rotation(math.cos(3 * pos))
+    #     pos = self.state[0]
+    #     self.cartrans.set_translation((pos-self.min_position)*scale, self._height(pos)*scale)
+    #     self.cartrans.set_rotation(math.cos(3 * pos))
 
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
+    #     return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
-    def close(self):
-        if self.viewer:
-            self.viewer.close()
-            self.viewer = None
+    # def close(self):
+    #     if self.viewer:
+    #         self.viewer.close()
+    #         self.viewer = None
