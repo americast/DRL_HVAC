6c714534a2e18f4b33e6e4a35a32de70cc0bc463
americast <sayan.sinha@iitkgp.ac.in>
2019-12-18 18:15:36 +0530

    Reward plot

--- DRL/main.py
+++ DRL/main.py
@@ -17,29 +17,32 @@ batch_size = 128
 rewards = []
 avg_rewards = []
 
-for episode in range(50):
-    state = env.reset()
-    noise.reset()
-    episode_reward = 0
-    
-    for step in range(500):
-        action = agent.get_action(state)
-        action = noise.get_action(action, step)
-        new_state, reward, done, _ = env.step(action) 
-        agent.memory.push(state, action, reward, new_state, done)
+try:
+    for episode in range(50):
+        state = env.reset()
+        noise.reset()
+        episode_reward = 0
         
-        if len(agent.memory) > batch_size:
-            agent.update(batch_size)        
-        
-        state = new_state
-        episode_reward += reward
+        for step in range(500):
+            action = agent.get_action(state)
+            action = noise.get_action(action, step)
+            new_state, reward, done, _ = env.step(action) 
+            agent.memory.push(state, action, reward, new_state, done)
+            
+            if len(agent.memory) > batch_size:
+                agent.update(batch_size)        
+            
+            state = new_state
+            episode_reward += reward
 
-        if step == 499:
-            sys.stdout.write("episode: {}, reward: {}, average _reward: {} \n".format(episode, np.round(episode_reward, decimals=2), np.mean(rewards[-10:])))
-            break
+            if step == 499:
+                sys.stdout.write("episode: {}, reward: {}, average _reward: {} \n".format(episode, np.round(episode_reward, decimals=2), np.mean(rewards[-10:])))
+                break
 
-    rewards.append(episode_reward)
-    avg_rewards.append(np.mean(rewards[-10:]))
+        rewards.append(episode_reward)
+        avg_rewards.append(np.mean(rewards[-10:]))
+except:
+    pass
 
 plt.plot(rewards)
 plt.plot(avg_rewards)
