eb91cb5d782003e463cdb542bf669e1a89aee607
americast <sayan.sinha@iitkgp.ac.in>
2020-05-30 02:18:30 +0530

    Fixes

--- DRL/plot_gen.py
+++ DRL/plot_gen.py
@@ -4,13 +4,14 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import math
 import argparse
+import numpy as np
 
 my_parser = argparse.ArgumentParser()
 my_parser.add_argument('-m', '--mpc', action='store_true', help="Also generate mpc plot results.")
 my_parser.add_argument('-c', '--combo', action='store_true', help="Also generate combo plot results.")
 args = my_parser.parse_args()
 mpc = vars(args)['mpc']
-combo = vars(args)['mpc']
+combo = vars(args)['combo']
 
 NUM_ZONES = 7
 all_weather = []
@@ -56,7 +57,7 @@ for i in tqdm(range(0, len(all_weather), 6)):
 	
 
 	for j in range(NUM_ZONES):
-		temp_now = all_temps[j] - 273.15 - 5
+		temp_now = all_temps[j] - 273.15 - 4.5
 		if mpc: temp_now_mpc = all_temps_mpc[j] - 273.15
 		if combo: temp_now_combo = all_temps_combo[j] - 273.15
 
@@ -88,7 +89,7 @@ for i in tqdm(range(0, len(all_weather), 6)):
 				zone_temp_diffs_combo[j].append(0)
 
 
-
+# pu.db
 for i, zone_temp in enumerate(zone_temps):
 	plt.plot(zone_temp, label = "DRL")
 	# plt.savefig("results/figs/temp_"+str(i + 1)+".png")
@@ -151,3 +152,89 @@ for i, zone_temp in enumerate(zone_temps):
 	plt.legend()
 	plt.savefig("results/figs/rmse_"+str(i + 1)+".png")
 	plt.clf()
+
+zone_temps = np.array(zone_temps)
+zone_temp_diffs = np.array(zone_temp_diffs)
+
+if mpc:
+	zone_temps_mpc = np.array(zone_temps_mpc)
+	zone_temp_diffs_mpc = np.array(zone_temp_diffs_mpc)
+
+if combo:
+	zone_temps_combo = np.array(zone_temps_combo)
+	zone_temp_diffs_combo = np.array(zone_temp_diffs_combo)
+
+zone_temp_avg = list(sum(zone_temps)/7)
+zone_temp_diffs_avg = list(sum(zone_temp_diffs)/7)
+
+if mpc:
+	zone_temp_avg_mpc = list(sum(zone_temps_mpc)/7)
+	zone_temp_diffs_avg_mpc = list(sum(zone_temp_diffs_mpc)/7)
+
+if combo:
+	zone_temp_avg_combo = list(sum(zone_temps_combo)/7)
+	zone_temp_diffs_avg_combo = list(sum(zone_temp_diffs_combo)/7)
+
+plt.plot(zone_temp_avg, label = "DRL")
+# plt.savefig("results/figs/temp_"+str(i + 1)+".png")
+if mpc:
+	plt.plot(zone_temp_avg_mpc, label = "MPC")
+if combo:
+	plt.plot(zone_temp_avg_combo, label = "Combo")
+		
+plt.legend()
+plt.savefig("results/figs/temp_avg.png")
+plt.clf()
+
+
+plt.plot(zone_temp_diffs_avg, label = "DRL")
+# plt.savefig("results/figs/power_"+str(i + 1)+".png")
+if mpc:
+	plt.plot(zone_temp_diffs_avg_mpc, label = "MPC")
+if combo:
+	plt.plot(zone_temp_diffs_avg_combo, label = "Combo")
+plt.legend()
+plt.savefig("results/figs/power_avg.png")
+plt.clf()
+
+
+rmse_here_avg = []
+positions = [x - 0.2 for x in range(1, 9)]
+positions_mpc = [x for x in range(1, 9)]
+positions_combo = [x + 0.2 for x in range(1, 9)]
+
+for j in range(0, len(zone_temp_avg), 6):
+	here_chunk = zone_temp_diffs_avg[j : j + 6]
+	here_chunk_sq = [x*x for x in here_chunk]
+	mean_here = sum(here_chunk_sq) / 6
+	rmse = mean_here ** 0.5
+	rmse_here_avg.append(rmse)
+
+if mpc:
+	rmse_here_avg_mpc = []
+	for j in range(0, len(zone_temp_avg_mpc), 6):
+		here_chunk_mpc = zone_temp_diffs_avg_mpc [j : j + 6]
+		here_chunk_sq_mpc = [x*x for x in here_chunk_mpc]
+		mean_here_mpc = sum(here_chunk_sq_mpc) / 6
+		rmse_mpc = mean_here_mpc ** 0.5
+		rmse_here_avg_mpc.append(rmse_mpc)
+
+if combo:
+	rmse_here_avg_combo = []
+	for j in range(0, len(zone_temp_avg_combo), 6):
+		here_chunk_combo = zone_temp_diffs_avg_combo [j : j + 6]
+		here_chunk_sq_combo = [x*x for x in here_chunk_combo]
+		mean_here_combo = sum(here_chunk_sq_combo) / 6
+		rmse_combo = mean_here_combo ** 0.5
+		rmse_here_avg_combo.append(rmse_combo)
+
+plt.bar(positions, rmse_here_avg, label = "DRL", width = 0.2)
+# plt.savefig("results/figs/rmse_"+str(i + 1)+".png")
+if mpc:
+	plt.bar(positions_mpc, rmse_here_avg_mpc, label = "MPC", width = 0.2)
+if combo:
+	plt.bar(positions_combo, rmse_here_avg_combo, label = "Combo", width = 0.2)
+
+plt.legend()
+plt.savefig("results/figs/rmse_avg.png")
+plt.clf()
